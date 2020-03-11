65ea4f2e45281a13741c5351fea1f55418203b09
gnizdyuh <ekstrumal@ukr.net>
2020-03-11 16:06:34 +0200

    find movie from trending list

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+const loading = (load) => {
+  if (load) {
+    document.querySelector('#search-results').innerHTML = `<div class="preloader-wrapper active">
+    <div class="spinner-layer spinner-blue-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div><div class="gap-patch">
+        <div class="circle"></div>
+      </div><div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>`;
+  }
+}
+
 const TrendingFilms = async () => {
+  loading(true);
   const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}`
   let response = await fetch(url)
   let films = await response.json()
   let collection = []
   await films.results.forEach((film) => {
-    collection.push(`<a class="collection-item">${film.title}</li>`)
+    collection.push(`<a class="collection-item" onclick="FindFilm('${film.id}')">${film.title}</li>`)
   })
   const container = document.querySelector('#search-results')
   container.innerHTML = `
@@ -12,25 +29,48 @@ const TrendingFilms = async () => {
       ${collection.join("")}
     </div>
   `
+  loading(false);
   return;
 }
 
-const FindHandler = async () => {
+const FindHandler = (e) => {
+  e.preventDefault()
   let input = document.querySelector('#search').value
-  if (!input.length) input = "*"
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${input}`
+  FindFilm(input)
+}
+
+const FindFilm = async (film) => {
+  loading(true)
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${film}`
+  if (film.match(/\d{6}/)) {
+    url = `https://api.themoviedb.org/3/movie/${film}?api_key=${api_key}&external_source=imdb_id`
+  }
+  if (!film.length) {
+    TrendingFilms();
+    return;
+  }
   let response = await fetch(url)
   let films = await response.json()
   const collection = document.querySelector('#search-results')
-  console.log(films)
+  loading(false)
   collection.innerHTML = DisplayFilm(films)
   document.querySelector('#search').value = ""
 }
 
-const DisplayFilm = ({ results }) => {
-  if (!results.length) {
+const DisplayFilm = (films) => {
+  let results
+  if (films.status_code === 34) {
     TrendingFilms()
     return;
+  }
+  if (films.results) {
+    if (!films.results.length || films.status_code === 34) {
+      TrendingFilms()
+      return;
+    }
+    results = films.results
+  } else {
+    results = [films]
   }
   let collection = []
   results.forEach(result => {
@@ -48,6 +88,5 @@ const DisplayFilm = ({ results }) => {
           </div>
         </li>`)
   })
-  console.log(collection)
   return `<ul class="collection">${collection.join("")}</ul>`
 }
